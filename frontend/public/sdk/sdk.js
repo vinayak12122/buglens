dec23c874dd2cbe60a92ef45f553f5894d3975c4
vnayak-mishra <vinayakportfolio@gmail.com>
2026-05-30 14:14:31 +0530

    Slight UI Updation

--- frontend/public/sdk/sdk.js
+++ frontend/public/sdk/sdk.js
@@ -1,14 +1,22 @@
 import { BugMonitorCore } from "./core.js";
 
-(()=>{
-    const script = document.querySelector(
-        'script[src*="sdk.js"]'
-    );
-    const apiKey = script?.dataset?.apiKey;
-    const endpoint = "https://buglens-tnzl.onrender.com/ingest/collect";
+(() => {
 
-    if(!apiKey){
-        throw new Error("BugMonitor: API Key missing");
+    const scripts = [...document.scripts];
+
+    const script = scripts.find(
+        s => s.dataset.apiKey
+    );
+
+    const apiKey = script?.dataset?.apiKey;
+
+    const endpoint =
+        "https://buglens-tnzl.onrender.com/ingest/collect";
+
+    if (!apiKey) {
+        throw new Error(
+            "BugMonitor: API Key missing"
+        );
     }
 
     const sdk = new BugMonitorCore({
@@ -18,4 +26,5 @@ import { BugMonitorCore } from "./core.js";
     });
 
     window.BugMonitor = sdk;
+
 })();
